387f4365aebb75557791aafd64bb3b3ca8920ef9
atshaw1994 <atshaw1994@gmail.com>
2025-12-01 19:15:11 -0500

    Initial Commit

--- obj/Debug/net9.0/RRT3SaveManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/RRT3SaveManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RRT3SaveManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889d6f38d62d12e8bb09cd64cb0141742a4a730b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RRT3SaveManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RRT3SaveManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
